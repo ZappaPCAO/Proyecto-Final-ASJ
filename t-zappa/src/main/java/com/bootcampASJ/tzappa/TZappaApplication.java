6abc4e6e6dd1a23039c7e68f4797dd613a4fbdfb
Tomas Zappala <zappala.tomas@hotmail.com>
2024-01-24 15:05:08 -0300

    creacion de back

---
+++ t-zappa/src/main/java/com/bootcampASJ/tzappa/TZappaApplication.java
@@ -0,0 +1,13 @@
+package com.bootcampASJ.tzappa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TZappaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TZappaApplication.class, args);
+	}
+
+}
---
+++ t-zappa/src/test/java/com/bootcampASJ/tzappa/TZappaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampASJ.tzappa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TZappaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
